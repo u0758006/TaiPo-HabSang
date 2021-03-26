d3991da7dd82e14367bbd343a4ede9d0e76a0498
Fafofafoy <ihcaoe@gmail.com>
2021-03-26 15:29:38 +0800

    加語言設定。

--- tshamsoo/symbols.py
+++ tshamsoo/symbols.py
@@ -4,16 +4,12 @@
 Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from utils.text import cmudict
 
 _pad = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_ipa = 'ŋəɛɨʔʰ̩ⁿ'
-
-_sooji = '0123456789'
+_letters = 'abcdefghijklmnopqrstuvwxyz'
+_tiau = 'ˊˇˋ^+'
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + list(_ipa) + list(_sooji)
-
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + list(_tiau)
