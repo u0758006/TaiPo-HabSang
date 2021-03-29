999c91c1bffbf79b223a688be0345eea37c68e2d
Fafofafoy <ihcaoe@gmail.com>
2021-03-29 11:21:25 +0800

    有半型括號。

--- tshamsoo/preprocess-tacotron.py
+++ tshamsoo/preprocess-tacotron.py
@@ -71,7 +71,7 @@ def khehue(path: Union[str, Path], wav_files):
                     if mia in u_tihleh:
                         hj = tsua['客家語']
                         lmj = tsua['客語標音']
-                        if '（' not in hj and '【' not in hj:
+                        if '（' not in hj and '(' not in hj and '【' not in hj:
                             text_dict[mia] = lmj
 
     return text_dict
